18865c2f8c8c099fc6d93999acb8eba2e53581ab
Prajwal Singh <prajwalrishabh@gmail.com>
2023-03-28 17:42:34 +0530

    Update config.py

--- vit_v1/config.py
+++ vit_v1/config.py
@@ -1,5 +1,6 @@
 # Reference:
 # Base MNIST Parameter: https://medium.com/mlearning-ai/vision-transformers-from-scratch-pytorch-a-step-by-step-guide-96c3313c2e0c
+# https://learnopencv.com/attention-mechanism-in-transformer-neural-networks/
 
 base_mnist={
 		'input_size': [1, 28, 28],
